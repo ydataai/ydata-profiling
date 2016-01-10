1c89425b39299517d78dc3830cfb264fb7027300
jos <jos.polfliet@gmail.com>
2016-01-09 19:57:35 -0500

    Create setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     author='Jos Polfliet',
     author_email='jos.polfliet+panpro@gmail.com',
-    packages=['pandas-profiling'],
+    packages=['pandas_profiling'],
     url='http://github.com/jospolfliet/pandas-profiling',
     license='LICENSE',
     description='Generate profile report for pandas DataFrame',
