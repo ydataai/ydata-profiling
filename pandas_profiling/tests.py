664f62a6cbbb3b5f0f997a140237de708837fee8
Jos Polfliet <jos@faction.xyz>
2019-04-27 09:48:56 +0200

    fix tests for adding check_recoded=True

--- pandas_profiling/tests.py
+++ pandas_profiling/tests.py
@@ -209,7 +209,6 @@ class DataFrameTest(unittest.TestCase):
             set(self.results['freq'].keys()), set(self.data.keys()))
         self.assertSetEqual(
             set(self.results['variables'].index), set(self.data.keys()))
-        print((self.results['table'].items()))
         self.assertTrue(set({
                                 'CAT': 2,
                                 'CONST': 2,
@@ -273,8 +272,7 @@ class CategoricalDataTest(unittest.TestCase):
              'y': ['dog', 'dog', 'dog', 'dog', 'cat', 'cat', 'camel', 'camel'],
            }
         self.df = pd.DataFrame(self.data)
-        self.results = describe(self.df)
-
+        self.results = describe(self.df, check_recoded=True)
         self.assertEqual(self.results['variables'].loc['x']['type'], 'RECODED')
         self.assertEqual(
             self.results['variables'].loc['x']['correlation_var'], 'y')
