131a2ccedf0050156d93704dc0021876edd32849
Simon Brugman <sbrugman@users.noreply.github.com>
2020-01-02 02:13:56 +0100

    Update __init__.py
    Black linting

--- pandas_profiling/__init__.py
+++ pandas_profiling/__init__.py
@@ -31,7 +31,7 @@ class ProfileReport(object):
     html = ""
     """the HTML representation of the report, without the wrapper (containing `<head>` etc.)"""
 
-    def __init__(self, df, config_file : Path = None, **kwargs):
+    def __init__(self, df, config_file: Path = None, **kwargs):
         if config_file:
             config.config.set_file(str(config_file))
         config.set_kwargs(kwargs)
