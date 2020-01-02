7722b2054e898e735a53a607468b0f914e5fdfbc
simon_graphkite <sfbbrugman@gmail.com>
2020-01-02 18:49:20 +0100

    Performance plot runtime complexity

--- tests/performance/timings.py
+++ tests/performance/timings.py
@@ -1,4 +1,3 @@
-import math
 import timeit
 from itertools import product
 from string import ascii_lowercase
@@ -11,20 +10,25 @@ import matplotlib.pyplot as plt
 from pandas_profiling import ProfileReport
 
 
-def make_sample_data(cols, rows):
-    iters = math.ceil(cols / 26)
+def generate_column_names(n):
     column_names = []
-    for x in range(1, iters + 1):
-        column_names += list(product(ascii_lowercase, repeat=x))
+    iters = 1
+    while len(column_names) < n:
+        column_names += list(''.join(combo) for combo in product(ascii_lowercase, repeat=iters))
+        iters += 1
+    return column_names
 
-    column_names = [x[0] for x in column_names]
+
+def make_sample_data(cols, rows):
+    column_names = generate_column_names(cols)
 
     df = pd.DataFrame(
         np.random.randint(0, 1000000, size=(rows, cols)), columns=column_names[0:cols]
     )
+    df = df.astype(str)
 
     assert df.shape == (rows, cols)
-    return df
+    return df.copy()
 
 
 def make_report_minimal(df):
@@ -32,6 +36,7 @@ def make_report_minimal(df):
         df,
         minimal=True,
         pool_size=0,
+        sort="None",
         title="Dataset with <em>Numeric</em> Categories",
     )
     html = report.to_html()
@@ -43,6 +48,7 @@ def make_report(df):
         df,
         minimal=False,
         pool_size=0,
+        sort="None",
         title="Dataset with <em>Numeric</em> Categories",
     )
     html = report.to_html()
@@ -61,20 +67,19 @@ def wrap_func(function):
 
 def time_report(func, cols, rows, runs=5):
     df = make_sample_data(cols, rows)
-    print(df.shape, cols, rows)
-    print(df.head())
-    test = wrap_func(func)(df)
+    print(df.shape)
+    test = wrap_func(func)(df.copy())
     return timeit.timeit(test, number=runs) / runs
 
 
 def plot_col_run_time():
-    cols = [1, 2, 4, 10, 50]
+    cols = [2, 4, 10, 50]
     row = 1000
     default_times = [time_report(make_report, col, row) for col in cols]
     minimal_times = [time_report(make_report_minimal, col, row) for col in cols]
 
     ax1 = sns.scatterplot(cols, default_times)
-    ax = sns.scatterplot(cols, minimal_times)
+    ax2 = sns.scatterplot(cols, minimal_times)
     _ = ax1.set(
         xlabel=f"Number of columns (row={row})",
         ylabel="time (s)",
@@ -102,5 +107,5 @@ def plot_row_run_time():
 
 
 if __name__ == "__main__":
-    # plot_col_run_time()
+    plot_col_run_time()
     plot_row_run_time()
