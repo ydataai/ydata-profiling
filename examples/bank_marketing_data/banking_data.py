1bdc853ebe156b04285eede8b61a9513648149ba
sbrugman <sfbbrugman@gmail.com>
2020-01-14 17:16:11 +0100

    Feature: Add Logo for more distinguishable reports

--- examples/bank_marketing_data/banking_data.py
+++ examples/bank_marketing_data/banking_data.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
     df = pd.read_csv(file_name, sep=";")
 
     profile = ProfileReport(
-        df, title="Profile Report of the UCI Bank Marketing Dataset"
+        df, title="UCI Bank Marketing Dataset"
     )
     profile.to_file("uci_bank_marketing_report.html")
