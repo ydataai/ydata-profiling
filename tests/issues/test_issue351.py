310d86550cbe278a4ea4078e948251665bcee9a6
Simon Brugman <sbrugman@users.noreply.github.com>
2020-02-02 20:11:52 +0100

    Fix and test for issue #351 (#357)
    Fix and test for issue #351

---
+++ tests/issues/test_issue351.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from pandas_profiling import ProfileReport
+from pandas_profiling.model.base import Variable
+
+
+def test_issue351():
+    data = pd.DataFrame(["Jan", 1]).set_index(0)
+
+    profile = ProfileReport(data)
+    assert (
+        profile.get_description()["variables"]["0"]["type"]
+        == Variable.S_TYPE_UNSUPPORTED
+    )
