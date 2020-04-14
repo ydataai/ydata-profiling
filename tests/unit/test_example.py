cc4aeeaaec8aee450615c0a40d314cc78ae3cd0d
loopyme <peter@mail.loopy.tech>
2020-04-14 12:49:07 +0800

    Update test: More types of warning are shown

--- tests/unit/test_example.py
+++ tests/unit/test_example.py
@@ -46,4 +46,4 @@ def test_example(get_data_file, test_output_dir):
         type(profile.get_description()) == dict
         and len(profile.get_description().items()) == 7
     ), "Unexpected result"
-    assert "<span class=badge>10</span>" in profile.to_html()
+    assert "<span class=badge>12</span>" in profile.to_html()
