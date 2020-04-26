0061ebf7e83cc34aa8dbb83301e5acc7dc8ee11d
sbrugman <sfbbrugman@gmail.com>
2020-04-26 14:03:21 +0200

    Fix test

--- tests/unit/test_type_inference.py
+++ tests/unit/test_type_inference.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 
-from pandas_profiling.model.base import get_var_type
+from pandas_profiling.model.base import get_var_type, Variable
 
 
 def test_numeric_with_inf():
-    s = pd.Series([1, 2, 3, 4, 5, 6, np.inf])
-    print(get_var_type(s))
+    s = pd.Series([1, 2, 3, 6, np.inf])
+    assert get_var_type(s)['type'] == Variable.TYPE_NUM
