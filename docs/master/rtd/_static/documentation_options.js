f291f08b61f622961c6568dae8684c21cf73666b
GitHub Action <action@github.com>
2020-05-11 18:00:07 +0000

    [skip ci] Updating documentation and examples

--- docs/master/rtd/_static/documentation_options.js
+++ docs/master/rtd/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.7.0',
+    VERSION: '2.7.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
