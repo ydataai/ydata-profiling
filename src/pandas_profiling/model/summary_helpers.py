f69f3df9b86b324ef03d7567edb9461d3c4d4ad0
sbrugman <sfbbrugman@gmail.com>
2020-09-14 03:24:06 +0200

    Fix warning

--- src/pandas_profiling/model/summary_helpers.py
+++ src/pandas_profiling/model/summary_helpers.py
@@ -255,7 +255,7 @@ def get_character_counts(series: pd.Series) -> Counter:
 
 def counter_to_series(counter: Counter) -> pd.Series:
     if not counter:
-        return pd.Series()
+        return pd.Series([], dtype=object)
 
     counter_as_tuples = counter.most_common()
     items, counts = zip(*counter_as_tuples)
