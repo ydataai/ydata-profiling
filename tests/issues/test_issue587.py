75428cbef4deabda9b3e24b889e95a2d71657a44
James Myatt <james@jamesmyatt.co.uk>
2020-09-25 13:00:00 +0100

    Create test_issue587.py

---
+++ tests/issues/test_issue587.py
@@ -0,0 +1,15 @@
+"""
+Test for issue 587:
+https://github.com/pandas-profiling/pandas-profiling/issues/587
+"""
+import pandas as pd
+
+import pandas_profiling
+from pandas_profiling.model.base import is_numeric
+
+
+def test_issue587():
+    # Minimal reproducible code
+    series = pandas.Series([1, None], dtype="Int64")
+    series_description = get_counts(series)
+    assert is_numeric(series, series_description) == True
