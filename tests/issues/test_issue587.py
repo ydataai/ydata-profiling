294837a0e47567d5d3f05c181df495811e3c50e4
James Myatt <james@jamesmyatt.co.uk>
2020-09-25 13:03:58 +0100

    Update test_issue587.py

--- tests/issues/test_issue587.py
+++ tests/issues/test_issue587.py
@@ -3,11 +3,14 @@ Test for issue 587:
 https://github.com/pandas-profiling/pandas-profiling/issues/587
 """
 import pandas as pd
+import pytest
 
-import pandas_profiling
 from pandas_profiling.model.base import is_numeric, get_counts
 
 
+@pytest.mark.skipif(
+    int(pd.__version__.split(".")[0]) < 1, reason="requires pandas 1 or higher"
+)
 def test_issue587():
     # Minimal reproducible code
     series = pd.Series([1, None], dtype="Int64")
