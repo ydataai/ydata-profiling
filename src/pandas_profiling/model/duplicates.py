e91998c0aed7125d06d69321060b5417704734e6
sbrugman <sfbbrugman@gmail.com>
2021-03-27 21:25:03 +0100

    Fix #725

--- src/pandas_profiling/model/duplicates.py
+++ src/pandas_profiling/model/duplicates.py
@@ -18,11 +18,18 @@ def get_duplicates(df: pd.DataFrame, supported_columns) -> Optional[pd.DataFrame
     n_head = config["duplicates"]["head"].get(int)
 
     if n_head > 0 and supported_columns:
+        duplicates_key = config["duplicates"]["key"].get(str)
+        if duplicates_key in df.columns:
+            raise ValueError(
+                f"Duplicates key ({duplicates_key}) may not be part of the DataFrame. Either change the "
+                f" column name in the DataFrame or change the 'duplicates.key' parameter."
+            )
+
         return (
             df[df.duplicated(subset=supported_columns, keep=False)]
             .groupby(supported_columns)
             .size()
-            .reset_index(name="count")
-            .nlargest(n_head, "count")
+            .reset_index(name=duplicates_key)
+            .nlargest(n_head, duplicates_key)
         )
     return None
