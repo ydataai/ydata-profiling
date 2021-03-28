d257b5e7e9df1de4df316af9b28ced8992f62f38
sbrugman <sfbbrugman@gmail.com>
2021-03-28 13:22:12 +0200

    refactor: simplify tests

--- tests/unit/test_summary_algos.py
+++ tests/unit/test_summary_algos.py
@@ -36,25 +36,18 @@ def empty_data() -> pd.DataFrame:
     return pd.DataFrame({"A": []})
 
 
-def test_count_summary_empty_df(empty_data):
-    _, result = describe_counts(empty_data["A"], {})
-    assert result["n_missing"] == 0
-    assert "p_missing" not in result
-
-
-def test_summary_generic_empty_df(empty_data):
-    series, summary = describe_counts(empty_data["A"], {})
-    _, result = describe_generic(series, summary)
-    assert result["n_missing"] == 0
-    assert result["p_missing"] == 0
-    assert result["count"] == 0
-
-
 def test_summary_supported_empty_df(empty_data):
     series, summary = describe_counts(empty_data["A"], {})
+    assert summary["n_missing"] == 0
+    assert "p_missing" not in summary
+
     series, summary = describe_generic(series, summary)
-    _, result = describe_supported(series, summary)
-    assert result["n_distinct"] == 0
-    assert result["p_distinct"] == 0
-    assert result["n_unique"] == 0
-    assert not result["is_unique"]
+    assert summary["n_missing"] == 0
+    assert summary["p_missing"] == 0
+    assert summary["count"] == 0
+
+    _, summary = describe_supported(series, summary)
+    assert summary["n_distinct"] == 0
+    assert summary["p_distinct"] == 0
+    assert summary["n_unique"] == 0
+    assert not summary["is_unique"]
