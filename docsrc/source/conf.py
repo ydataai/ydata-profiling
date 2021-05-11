f335461ab26caa82ac83c0de7b00ae20f2cf880f
sbrugman <sfbbrugman@gmail.com>
2021-05-11 15:18:41 +0200

    refactor: remove unused pattern

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -59,7 +59,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "asdasd"]
+exclude_patterns = ["_build"]
 
 master_doc = "index"
 
