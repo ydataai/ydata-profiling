3732afdf28b329d680f80ffd1865230a94db7786
sbrugman <sfbbrugman@gmail.com>
2021-05-14 23:25:37 +0200

    perf: categorical 5-50x speedup in explorative mode
    unicode, length, words

---
+++ src/pandas_profiling/model/pandas/utils_pandas.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def weighted_median(data: np.ndarray, weights: np.ndarray) -> int:
+    """
+    Args:
+      data (list or numpy.array): data
+      weights (list or numpy.array): weights
+    """
+    s_data, s_weights = map(np.array, zip(*sorted(zip(data, weights))))
+    midpoint = 0.5 * sum(s_weights)
+    if any(weights > midpoint):
+        w_median = (data[weights == np.max(weights)])[0]
+    else:
+        cs_weights = np.cumsum(s_weights)
+        idx = np.where(cs_weights <= midpoint)[0][-1]
+        if cs_weights[idx] == midpoint:
+            w_median = np.mean(s_data[idx : idx + 2])
+        else:
+            w_median = s_data[idx + 1]
+    return w_median
