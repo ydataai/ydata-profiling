4452e6575283e15873794a50c953ec88f3762a83
Ian Eaves <ian.k.eaves@gmail.com>
2022-02-18 18:20:02 -0600

    fix

--- src/pandas_profiling/model/pandas/utils_pandas.py
+++ src/pandas_profiling/model/pandas/utils_pandas.py
@@ -10,7 +10,7 @@ def weighted_median(data: np.ndarray, weights: np.ndarray) -> int:
     s_data, s_weights = map(np.array, zip(*sorted(zip(data, weights))))
     midpoint = 0.5 * sum(s_weights)
     if any(weights > midpoint):
-        w_median = (data[weights == np.max(weights)])[0]
+        w_median = (data[np.where(weights == np.max(weights))])[0]
     else:
         cs_weights = np.cumsum(s_weights)
         idx = np.where(cs_weights <= midpoint)[0][-1]
