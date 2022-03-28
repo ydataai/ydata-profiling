7394bfce44e1588f0d71920035e31582a655a10c
Endre Márk Borza <endremborza@gmail.com>
2022-03-28 23:10:30 +0200

    fix: possibly align with index sorting

--- tests/unit/test_pandas/test_describe_categorical_pandas.py
+++ tests/unit/test_pandas/test_describe_categorical_pandas.py
@@ -7,15 +7,17 @@ value_counts_w_words = pd.Series(index=["The dog", "is hungry"], data=[2, 1])
 
 
 def test_word_summary_vc():
-    pd.testing.assert_series_equal(
-        word_summary_vc(vc=value_counts_w_words)["word_counts"],
-        pd.Series(index=["the", "dog", "is", "hungry"], data=[2, 2, 1, 1]),
+    assert (
+        word_summary_vc(vc=value_counts_w_words)["word_counts"].to_dict()
+        == pd.Series(index=["the", "dog", "is", "hungry"], data=[2, 2, 1, 1]).to_dict()
     )
 
 
 @pytest.mark.parametrize("stop_words", [["The"], ["the", "a"]])
 def test_word_summary_vc_with_stop_words(stop_words):
-    pd.testing.assert_series_equal(
-        word_summary_vc(vc=value_counts_w_words, stop_words=stop_words)["word_counts"],
-        pd.Series(index=["dog", "is", "hungry"], data=[2, 1, 1]),
+    assert (
+        word_summary_vc(vc=value_counts_w_words, stop_words=stop_words)[
+            "word_counts"
+        ].to_dict()
+        == pd.Series(index=["dog", "is", "hungry"], data=[2, 1, 1]).to_dict()
     )
