43c34b7df070921672b4ef984d49e1d73c32e2d6
James Took <james.m.took@gmail.com>
2022-10-04 13:45:48 +0100

    docs: improve documentation

--- src/pandas_profiling/report/structure/correlations.py
+++ src/pandas_profiling/report/structure/correlations.py
@@ -55,11 +55,12 @@ def get_correlation_items(config: Settings, summary: dict) -> Optional[Renderabl
     The empirical estimators used for Cramér's V have been proved to be biased, even for large samples.
     We use a bias-corrected measure that has been proposed by Bergsma in 2013 that can be found <a href='http://stats.lse.ac.uk/bergsma/pdf/cramerV3.pdf'>here</a>."""
 
-    association_description = """The association metric is an easily interpretable pairwise column metric of the following mapping:
+    auto_description = """The auto setting is an easily interpretable pairwise column metric of the following mapping:
                                 vartype-vartype         : method, 
                                 categorical-categorical : Cramer's V, 
                                 numerical-categorical   : Cramer's V (using a discretized numerical column), 
-                                Ordinal-Ordinal         : Spearman's ρ. """
+                                numerical-numerical     : Spearman's ρ. 
+                                This configuration uses the best metric for each pair of columns."""
 
     key_to_data = {
         "pearson": (-1, "Pearson's r", pearson_description),
@@ -67,7 +68,7 @@ def get_correlation_items(config: Settings, summary: dict) -> Optional[Renderabl
         "kendall": (-1, "Kendall's τ", kendall_description),
         "phi_k": (0, "Phik (φk)", phi_k_description),
         "cramers": (0, "Cramér's V (φc)", cramers_description),
-        "association": (0, "Association", association_description),
+        "auto": (0, "Auto", auto_description),
     }
 
     image_format = config.plot.image_format
