cba8ec52c9c801fb95360739f372f8b14cd595c3
Vasco Ramos <vasco.ramos@ydata.ai>
2022-10-18 17:11:35 +0100

    fix(actions): add version.py generation

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ try:
 except FileNotFoundError:
     version = "0.0.0"
 
-print(version)
+with open(source_root / "src/pandas_profiling/version.py", "w") as version_file:
+  version_file.write(f'__version__ = \'{version}\'')
 
 setup(
     name="pandas-profiling",
