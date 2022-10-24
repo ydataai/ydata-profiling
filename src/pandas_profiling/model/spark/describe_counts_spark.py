5f888c0f4af1ba0afd5bf3d99fcc4c71a5413f36
Edwin Chan <edwin.ke.chan@gmail.com>
2022-10-24 22:05:59 +0800

    refactor: lint fixes

--- src/pandas_profiling/model/spark/describe_counts_spark.py
+++ src/pandas_profiling/model/spark/describe_counts_spark.py
@@ -29,17 +29,6 @@ def describe_counts_spark(
     else:
         n_missing = n_missing["count"]
 
-    # max number of rows to visualise on histogram, most common values taken
-    # FIXME: top-n parameter
-    # to_pandas_limit = 100
-    # limited_results = (
-    #     value_counts.orderBy("count", ascending=False).limit(to_pandas_limit).toPandas()
-    # )
-
-    # limited_results = limited_results.set_index(series.columns[0], drop=True).squeeze(
-    #     axis="columns"
-    # )
-
     # FIXME: reduce to top-n and bottom-n
     value_counts_index_sorted = (
         value_counts_index_sorted.limit(200)
