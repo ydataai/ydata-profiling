3cf3aac2eba7e269431ce60fbe35b0a5681652f5
Edwin Chan <edwin.ke.chan@gmail.com>
2022-10-24 11:36:36 +0800

    fix: swap order of computations
    correlations needs to check that the dataframe is not empty.
    
    This does the table-based computations first so
    that summary["n"] which
    counts number of real rows
    is available for correlations.

--- src/pandas_profiling/model/describe.py
+++ src/pandas_profiling/model/describe.py
@@ -88,14 +88,21 @@ def describe(
         ]
         pbar.update()
 
+        # Table statistics
+        table_stats = progress(get_table_stats, pbar, "Get dataframe statistics")(
+            config, df, series_description
+        )
+
         # Get correlations
         correlation_names = get_active_correlations(config)
         pbar.total += len(correlation_names)
 
         correlations = {
             correlation_name: progress(
-                calculate_correlation, pbar, f"Calculate {correlation_name} correlation"
-            )(config, df, correlation_name, series_description)
+                calculate_correlation,
+                pbar,
+                f"Calculate {correlation_name} correlation",
+            )(config, df, correlation_name, table_stats)
             for correlation_name in correlation_names
         }
 
@@ -115,11 +122,6 @@ def describe(
             scatter_matrix[x][y] = progress(
                 get_scatter_plot, pbar, f"scatter {x}, {y}"
             )(config, df, x, y, interval_columns)
-
-        # Table statistics
-        table_stats = progress(get_table_stats, pbar, "Get dataframe statistics")(
-            config, df, series_description
-        )
 
         # missing diagrams
         missing_map = get_missing_active(config, table_stats)
