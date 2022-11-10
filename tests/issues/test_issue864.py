a992a2ee3409482788887497bfed2a412772c20a
Alex Barros <alexbarros@users.noreply.github.com>
2022-11-10 09:07:24 -0300

    fix: update repository links (#1141)

--- tests/issues/test_issue864.py
+++ tests/issues/test_issue864.py
@@ -1,6 +1,6 @@
 """
 Test for issue 864:
-https://github.com/pandas-profiling/pandas-profiling/issues/
+https://github.com/ydataai/pandas-profiling/issues/
 
 Validate Extreme Values sub-tabs state the correct number of extreme values shown.
 """
