fc5da9eff07e7e18c5fd2d8caa698af7aee861e2
ricardodcpereira <ricardo.pereira@ydata.ai>
2022-12-05 12:32:18 +0000

    feat: add heatmap values as a table under correlations

---
+++ src/pandas_profiling/report/presentation/flavours/widget/correlation_table.py
@@ -0,0 +1,14 @@
+from IPython.core.display import display
+from ipywidgets import Output, widgets
+
+from pandas_profiling.report.presentation.core.correlation_table import CorrelationTable
+
+
+class WidgetCorrelationTable(CorrelationTable):
+    def render(self) -> widgets.VBox:
+        out = Output()
+        with out:
+            display(self.content["correlation_matrix"])
+
+        name = widgets.HTML(f"<h4>{self.content['name']}</h4>")
+        return widgets.VBox([name, out])
