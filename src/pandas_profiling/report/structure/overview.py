f2edcd2f0e111932d4104acb623ee8cd7eef6d0e
ricardodcpereira <ricardo.pereira@ydata.ai>
2022-12-06 14:58:22 +0000

    fix: compare report warnings always showing the last alert type

--- src/pandas_profiling/report/structure/overview.py
+++ src/pandas_profiling/report/structure/overview.py
@@ -227,9 +227,9 @@ def get_dataset_alerts(config: Settings, alerts: list) -> Alerts:
         count = 0
 
         # Initialize
-        no_alerts = [None for _ in range(len(alerts))]
         combined_alerts = {
-            f"{alert.alert_type}_{alert.column_name}": no_alerts
+            f"{alert.alert_type}_{alert.column_name}": 
+                [None for _ in range(len(alerts))]
             for report_alerts in alerts
             for alert in report_alerts
         }
@@ -247,7 +247,7 @@ def get_dataset_alerts(config: Settings, alerts: list) -> Alerts:
                     if alert.alert_type != AlertType.REJECTED
                 ]
             )
-
+        
         return Alerts(
             alerts=combined_alerts,
             name=f"Alerts ({count})",
