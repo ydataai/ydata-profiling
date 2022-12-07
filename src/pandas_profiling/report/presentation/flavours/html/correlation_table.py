e5cd8cfb4b91f22b3435f9830f516e929c4e8d32
ricardodcpereira <ricardo.pereira@ydata.ai>
2022-12-07 10:54:19 +0000

    feat: design improvements on the correlations section

--- src/pandas_profiling/report/presentation/flavours/html/correlation_table.py
+++ src/pandas_profiling/report/presentation/flavours/html/correlation_table.py
@@ -5,7 +5,7 @@ from pandas_profiling.report.presentation.flavours.html import templates
 class HTMLCorrelationTable(CorrelationTable):
     def render(self) -> str:
         correlation_matrix_html = self.content["correlation_matrix"].to_html(
-            classes="correlation-table table table-striped"
+            classes="correlation-table table table-striped", float_format="{:.3f}".format
         )
         return templates.template("correlation_table.html").render(
             **self.content, correlation_matrix_html=correlation_matrix_html
