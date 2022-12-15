f70b6532daa896ff7ddf000ab83032d291ae0ad1
Alex Barros <alexbarros@users.noreply.github.com>
2022-12-15 08:33:02 -0300

    feat: update variables layout (#1207)
    * feat: update variables info layout
    
    * fix: varname alignment and warnings placement
    
    * fix: histogram layout
    
    * [skip ci] Code formatting
    
    * fix: rename variable for readability
    
    Co-authored-by: Azory YData Bot <azory@ydata.ai>

--- src/pandas_profiling/visualisation/plot.py
+++ src/pandas_profiling/visualisation/plot.py
@@ -31,6 +31,7 @@ def _plot_histogram(
     figsize: tuple = (6, 4),
     date: bool = False,
     hide_yaxis: bool = False,
+    vertically: bool = True,
 ) -> plt.Figure:
     """Plot a histogram from the data and return the AxesSubplot object.
 
@@ -40,6 +41,7 @@ def _plot_histogram(
         bins: number of bins (int for equal size, ndarray for variable size)
         figsize: The size of the figure (width, height) in inches, default (6,4)
         date: is the x-axis of date type
+        vertically: display the histograms vertically if true, and horizontally otherwhise
 
     Returns:
         The histogram plot.
