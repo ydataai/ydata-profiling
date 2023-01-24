8d23d82fdee137f04e58eaff6e2f12dfdbc6070b
Fabiana Clemente <fabianaclemente@Fabianas-MacBook-Air.local>
2023-01-23 21:16:50 -0800

    fix: fix date and datetime histogram. Fix Spearman and Pearson's correlations

--- src/pandas_profiling/model/spark/describe_date_spark.py
+++ src/pandas_profiling/model/spark/describe_date_spark.py
@@ -1,9 +1,26 @@
 from typing import Tuple
 
+import pyspark.sql.functions as F
+from numpy import array, inf
 from pyspark.sql import DataFrame
 
 from pandas_profiling.config import Settings
-from pandas_profiling.model.summary_algorithms import describe_date_1d
+from pandas_profiling.model.summary_algorithms import (
+    describe_date_1d,
+    histogram_compute,
+)
+
+
+def date_stats_spark(df: DataFrame, summary: dict) -> dict:
+    print("calculate stats for date/datetime")
+    column = df.columns[0]
+
+    expr = [
+        F.min(F.col(column)).alias("min"),
+        F.max(F.col(column)).alias("max"),
+    ]
+
+    return df.agg(*expr).first().asDict()
 
 
 @describe_date_1d.register
@@ -19,5 +36,22 @@ def describe_date_1d_spark(
     Returns:
         A dict containing calculated series description values.
     """
+    col_name = df.columns[0]
+    stats = date_stats_spark(df, summary)
 
+    summary.update({"min": stats["min"], "max": stats["max"]})
+
+    summary["range"] = summary["max"] - summary["min"]
+
+    # Convert date to numeric so we can compute the histogram
+    df = df.withColumn(col_name, F.unix_timestamp(df[col_name]))
+
+    # Get the number of bins
+    bins = config.plot.histogram.bins
+    bins_arg = "auto" if bins == 0 else min(bins, summary["n_distinct"])
+
+    # Run the histogram
+    bin_edges, hist = df.select(col_name).rdd.flatMap(lambda x: x).histogram(bins_arg)
+
+    summary.update({"histogram": (array(hist), array(bin_edges))})
     return config, df, summary
