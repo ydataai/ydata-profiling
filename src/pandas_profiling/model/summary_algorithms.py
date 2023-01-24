6a5409e6fa9f87fcfea60bda5632ca71462de791
Sohaib Arshid <sohaib.arshid101@gmail.com>
2023-01-24 15:01:05 +0100

    fix: issue#915 rename var bin_args

--- src/pandas_profiling/model/summary_algorithms.py
+++ src/pandas_profiling/model/summary_algorithms.py
@@ -41,8 +41,8 @@ def histogram_compute(
 
     max_bins = config.plot.histogram.max_bins
     if bins_arg == "auto" and len(stats[name][1]) > max_bins:
-        bins_arg = np.histogram_bin_edges(finite_values, bins=max_bins)
-        stats[name] = np.histogram(finite_values, bins=bins_arg, weights=None)
+        bins = np.histogram_bin_edges(finite_values, bins=max_bins)
+        stats[name] = np.histogram(finite_values, bins=bins, weights=None)
 
     return stats
 
