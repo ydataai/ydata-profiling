61e1adf0db73045fe0a66ce0bafb0fd5cf60f107
Alexandre Quemy <alexandre.quemy@gmail.com>
2023-01-30 14:10:54 +0100

    fix: revert the URL of a test file

--- tests/issues/test_issue120.py
+++ tests/issues/test_issue120.py
@@ -10,7 +10,7 @@ from ydata_profiling import ProfileReport
 def test_issue_120(get_data_file):
     file_name = get_data_file(
         "ydata_profiling_bug.txt",
-        "https://github.com/ydataai/ydata-profiling/files/2386812/ydata_profiling_bug.txt",
+        "https://github.com/pandas-profiling/pandas-profiling/files/2386812/pandas_profiling_bug.txt",
     )
     df = pd.read_csv(file_name)
 
