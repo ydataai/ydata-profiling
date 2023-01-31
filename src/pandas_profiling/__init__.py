8fc9a815b628a3f6793af3a067709ee0a512c04c
Vasco Ramos <vasco.ramos@ydata.ai>
2023-01-31 11:13:21 +0000

    chore: reintroduce missing depcreation warning

--- src/pandas_profiling/__init__.py
+++ src/pandas_profiling/__init__.py
@@ -3,6 +3,8 @@
 .. include:: ../../README.md
 """
 
+from warnings import warn
+
 from pandas_profiling.compare_reports import compare
 from pandas_profiling.controller import pandas_decorator
 from pandas_profiling.profile_report import ProfileReport
@@ -11,6 +13,11 @@ from pandas_profiling.version import __version__
 # backend
 import pandas_profiling.model.pandas  # isort:skip  # noqa
 
+warn(
+    "`import pandas_profiling` is going to be deprecated by April 1st. Please use `import ydata_profiling` instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
 
 __all__ = [
     "pandas_decorator",
