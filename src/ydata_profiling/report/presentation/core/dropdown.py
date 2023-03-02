cf9dac7b2932d7723442b741d409e3bf40b2ab21
Alexandre Quemy <alexandre.quemy@ydata.ai>
2023-03-02 10:13:07 +0100

    feat: support for Python 3.11 (#1273)
    * feat: support for Python 3.11
    
    * fix: type the exception, remove useless import
    
    * [skip ci] Code formatting
    
    ---------
    
    Co-authored-by: Azory YData Bot <azory@ydata.ai>

--- src/ydata_profiling/report/presentation/core/dropdown.py
+++ src/ydata_profiling/report/presentation/core/dropdown.py
@@ -7,7 +7,15 @@ from ydata_profiling.report.presentation.core.renderable import Renderable
 
 class Dropdown(ItemRenderer):
     def __init__(
-        self, name: str, id: str, items: list, item: Container, anchor_id: str, classes: list, is_row: bool, **kwargs
+        self,
+        name: str,
+        id: str,
+        items: list,
+        item: Container,
+        anchor_id: str,
+        classes: list,
+        is_row: bool,
+        **kwargs
     ):
         super().__init__(
             "dropdown",
