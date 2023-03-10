2fc6bb22716a95882cda8354fd715481cd7ff397
Jan Cap <vorel007@gmail.com>
2023-03-10 18:02:10 +0100

    add describe string for spark
    - same as category

---
+++ src/ydata_profiling/model/spark/describe_string_spark.py
@@ -0,0 +1,27 @@
+from typing import Tuple
+
+from pyspark.sql import DataFrame
+from ydata_profiling.config import Settings
+from ydata_profiling.model.summary_algorithms import describe_string_1d
+
+
+@describe_string_1d.register
+def describe_string_1d_spark(
+    config: Settings, df: DataFrame, summary: dict
+) -> Tuple[Settings, DataFrame, dict]:
+    """Describe a categorical series.
+
+    Args:
+        series: The Series to describe.
+        summary: The dict containing the series description so far.
+
+    Returns:
+        A dict containing calculated series description values.
+    """
+
+    # FIXME: cat description
+    redact = config.vars.cat.redact
+    if not redact:
+        summary["first_rows"] = df.limit(5).toPandas().squeeze("columns")
+
+    return config, df, summary
