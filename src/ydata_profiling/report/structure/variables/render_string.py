bfdb5efeaea31734275ddef362dbc5bf6e0fe71d
Jan Cap <vorel007@gmail.com>
2023-03-14 22:37:37 +0100

    format string render
    - update string render to same format as other renders

--- src/ydata_profiling/report/structure/variables/render_string.py
+++ src/ydata_profiling/report/structure/variables/render_string.py
@@ -8,7 +8,7 @@ from ydata_profiling.report.presentation.core import (
     Image,
     Table,
 )
-from ydata_profiling.report.structure.variables.base_render import BaseRenderVariable
+from ydata_profiling.report.presentation.core.variable_info import VariableInfo
 from ydata_profiling.report.structure.variables.render_categorical import (
     _get_n,
     freq_table,
@@ -16,203 +16,170 @@ from ydata_profiling.report.structure.variables.render_categorical import (
     render_categorical_length,
     render_categorical_unicode,
 )
+from ydata_profiling.report.structure.variables.render_common import render_common
 from ydata_profiling.visualisation.plot import plot_word_cloud
 
 
-class RenderString(BaseRenderVariable):
-    @property
-    def varid(self):
-        return self.summary["varid"]
+def render_string(config: Settings, summary: Dict[str, Any]):
+    varid = summary["varid"]
+    image_format = config.plot.image_format
+    words = config.vars.str.words
+    characters = config.vars.str.characters
+    length = config.vars.str.length
 
-    @property
-    def words(self) -> bool:
-        return self.config.vars.str.words
+    template_variables = render_common(config, summary)
 
-    @property
-    def characters(self) -> bool:
-        return self.config.vars.str.characters
+    top_items = []
+    var_info = VariableInfo(
+        anchor_id=varid,
+        var_name=summary["varname"],
+        var_type=summary["type"],
+        alerts=summary["alerts"],
+        description=summary["description"],
+        style=config.html.style,
+    )
+    top_items.append(var_info)
 
-    @property
-    def length(self) -> bool:
-        return self.config.vars.cat.length
+    table = Table(
+        [
+            {
+                "name": "Distinct",
+                "value": fmt(summary["n_distinct"]),
+                "alert": "n_distinct" in summary["alert_fields"],
+            },
+            {
+                "name": "Distinct (%)",
+                "value": fmt_percent(summary["p_distinct"]),
+                "alert": "p_distinct" in summary["alert_fields"],
+            },
+            {
+                "name": "Missing",
+                "value": fmt(summary["n_missing"]),
+                "alert": "n_missing" in summary["alert_fields"],
+            },
+            {
+                "name": "Missing (%)",
+                "value": fmt_percent(summary["p_missing"]),
+                "alert": "p_missing" in summary["alert_fields"],
+            },
+            {
+                "name": "Memory size",
+                "value": fmt_bytesize(summary["memory_size"]),
+                "alert": False,
+            },
+        ],
+        style=config.html.style,
+    )
+    top_items.append(table)
 
-    def _get_top(self) -> Container:
-        """Render top of string variable.
-
-        Contains
-        - basic info
-        - table with information about missing values
-        - word cloud graph"""
-        top_items = []
-        top_items.append(self._get_info())
-        table = Table(
-            [
-                {
-                    "name": "Distinct",
-                    "value": fmt(self.summary["n_distinct"]),
-                    "alert": "n_distinct" in self.summary["alert_fields"],
-                },
-                {
-                    "name": "Distinct (%)",
-                    "value": fmt_percent(self.summary["p_distinct"]),
-                    "alert": "p_distinct" in self.summary["alert_fields"],
-                },
-                {
-                    "name": "Missing",
-                    "value": fmt(self.summary["n_missing"]),
-                    "alert": "n_missing" in self.summary["alert_fields"],
-                },
-                {
-                    "name": "Missing (%)",
-                    "value": fmt_percent(self.summary["p_missing"]),
-                    "alert": "p_missing" in self.summary["alert_fields"],
-                },
-                {
-                    "name": "Memory size",
-                    "value": fmt_bytesize(self.summary["memory_size"]),
-                    "alert": False,
-                },
-            ],
-            style=self.config.html.style,
+    if words and "word_counts" in summary:
+        mini_wordcloud = Image(
+            plot_word_cloud(config, summary["word_counts"]),
+            image_format=config.plot.image_format,
+            alt="Mini wordcloud",
         )
-        top_items.append(table)
+        top_items.append(mini_wordcloud)
+    template_variables["top"] = Container(top_items, sequence_type="grid")
 
-        if self.words and "word_counts" in self.summary:
-            mini_wordcloud = Image(
-                plot_word_cloud(self.config, self.summary["word_counts"], mini=False),
-                image_format=self.config.plot.image_format,
-                alt="Mini wordcloud",
+    # ============================================================================================
+
+    bottom_items = []
+    overview_items = []
+    # length isn't being computed for categorical in spark
+    if length and "max_length" in summary:
+        length_table, length_histo = render_categorical_length(config, summary, varid)
+        overview_items.append(length_table)
+
+    # characters isn't being computed for categorical in spark
+    unitab = None
+    if characters and "category_alias_counts" in summary:
+        overview_table_char, unitab = render_categorical_unicode(config, summary, varid)
+        overview_items.append(overview_table_char)
+
+    unique_stats = render_categorical_frequency(config, summary, varid)
+    overview_items.append(unique_stats)
+
+    if not config.vars.str.redact:
+        rows = ("1st row", "2nd row", "3rd row", "4th row", "5th row")
+
+        if isinstance(summary["first_rows"], list):
+            sample = Table(
+                [
+                    {
+                        "name": name,
+                        "value": fmt(value),
+                        "alert": False,
+                    }
+                    for name, *value in zip(rows, *summary["first_rows"])
+                ],
+                name="Sample",
+                style=config.html.style,
             )
-            top_items.append(mini_wordcloud)
-        return Container(top_items, sequence_type="grid")
-
-    def _get_overview(self) -> Container:
-        """Render overview container.
-
-        Contains
-        - string length
-        - unique statistic
-        - sample of data"""
-        overview_items = []
-        # length isn't being computed for categorical in spark
-        if self.length and "max_length" in self.summary:
-            length_table, length_histo = render_categorical_length(
-                self.config, self.summary, self.summary["varid"]
+        else:
+            sample = Table(
+                [
+                    {
+                        "name": name,
+                        "value": fmt(value),
+                        "alert": False,
+                    }
+                    for name, value in zip(rows, summary["first_rows"])
+                ],
+                name="Sample",
+                style=config.html.style,
             )
-            overview_items.append(length_table)
+        overview_items.append(sample)
+    overview = Container(
+        overview_items,
+        name="Overview",
+        anchor_id=f"{varid}overview",
+        sequence_type="batch_grid",
+        batch_size=len(overview_items),
+        titles=False,
+    )
+    bottom_items.append(overview)
 
-        # characters isn't being computed for categorical in spark
-        if self.characters and "category_alias_counts" in self.summary:
-            overview_table_char, self._unitab = render_categorical_unicode(
-                self.config, self.summary, self.varid
-            )
-            overview_items.append(overview_table_char)
-
-        unique_stats = render_categorical_frequency(
-            self.config, self.summary, self.summary["varid"]
-        )
-        overview_items.append(unique_stats)
-
-        if not self.config.vars.str.redact:
-            rows = ("1st row", "2nd row", "3rd row", "4th row", "5th row")
-
-            if isinstance(self.summary["first_rows"], list):
-                sample = Table(
-                    [
-                        {
-                            "name": name,
-                            "value": fmt(value),
-                            "alert": False,
-                        }
-                        for name, *value in zip(rows, *self.summary["first_rows"])
-                    ],
-                    name="Sample",
-                    style=self.config.html.style,
-                )
-            else:
-                sample = Table(
-                    [
-                        {
-                            "name": name,
-                            "value": fmt(value),
-                            "alert": False,
-                        }
-                        for name, value in zip(rows, self.summary["first_rows"])
-                    ],
-                    name="Sample",
-                    style=self.config.html.style,
-                )
-            overview_items.append(sample)
-        return Container(
-            overview_items,
-            name="Overview",
-            anchor_id="{}overview".format(self.summary["varid"]),
-            sequence_type="batch_grid",
-            batch_size=len(overview_items),
-            titles=False,
-        )
-
-    def _get_words(self) -> Container:
-        """Render words container.
-
-        Contains
-        - frequency table of words
-        - word map"""
+    if words and "word_counts" in summary:
         woc = freq_table(
-            freqtable=self.summary["word_counts"],
-            n=_get_n(self.summary["word_counts"]),
+            freqtable=summary["word_counts"],
+            n=_get_n(summary["word_counts"]),
             max_number_to_print=10,
         )
 
         fqwo = FrequencyTable(
             woc,
             name="Common words",
-            anchor_id="{}cwo".format(self.summary["varid"]),
-            redact=self.config.vars.cat.redact,
+            anchor_id=f"{varid}cwo",
+            redact=config.vars.str.redact,
         )
 
         image = Image(
-            plot_word_cloud(self.config, self.summary["word_counts"]),
-            image_format=self.config.plot.image_format,
+            plot_word_cloud(config, summary["word_counts"]),
+            image_format=config.plot.image_format,
             alt="Wordcloud",
         )
 
-        return Container(
-            [fqwo, image],
-            name="Words",
-            anchor_id="{}word".format(self.summary["varid"]),
-            sequence_type="grid",
+        bottom_items.append(
+            Container(
+                [fqwo, image],
+                name="Words",
+                anchor_id=f"{varid}word",
+                sequence_type="grid",
+            )
         )
 
-    def _get_characters(self) -> Container:
-        return Container(
-            [self._unitab],
-            name="Characters",
-            anchor_id=f"{self.varid}characters",
-            sequence_type="grid",
+    if unitab is not None:
+        bottom_items.append(
+            Container(
+                [unitab],
+                name="Characters",
+                anchor_id=f"{varid}characters",
+                sequence_type="grid",
+            )
         )
 
-    def _get_bottom(self) -> Container:
-        """Render bottom section of variable.
+    template_variables["bottom"] = Container(
+        bottom_items, sequence_type="tabs", anchor_id=f"{varid}bottom"
+    )
 
-        Contains
-        - overview container
-        - words container"""
-        bottom_items = []
-        bottom_items.append(self._get_overview())
-        if self.words and "word_counts" in self.summary:
-            bottom_items.append(self._get_words())
-
-        if self._unitab is not None:
-            bottom_items.append(self._get_characters())
-
-        return Container(
-            bottom_items,
-            sequence_type="tabs",
-            anchor_id="{}bottom".format(self.summary["varid"]),
-        )
-
-
-def render_string(config: Settings, summary: Dict[str, Any]):
-    render = RenderString(config, summary).render()
-    return render
+    return template_variables
