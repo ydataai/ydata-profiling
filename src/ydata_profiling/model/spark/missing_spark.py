fix: remove computation from missing data plots (#1294)

src/ydata_profiling/model/spark/missing_spark.py
@@ -1,5 +1,6 @@
 from typing import Any, List, Optional
 
+import numpy as np
 from pyspark.sql import DataFrame
 
 from ydata_profiling.config import Settings
@@ -67,18 +68,33 @@ def spark_missing_bar(config: Settings, df: DataFrame) -> str:
     )
 
     return plot_missing_bar(
-        config,
-        MissingnoBarSparkPatch(
-            df=data_nan_counts, columns=df.columns, original_df_size=df.count()
-        ),
+        config, notnull_counts=data_nan_counts, columns=df.columns, nrows=df.count()
     )
 
 
 @missing_matrix.register
 def spark_missing_matrix(config: Settings, df: DataFrame) -> str:
-    return plot_missing_matrix(config, MissingnoBarSparkPatch(df))
+    df = MissingnoBarSparkPatch(df, columns=df.columns, original_df_size=df.count())
+    return plot_missing_matrix(
+        config,
+        columns=df.columns,
+        notnull=df.notnull().values,
+        nrows=len(df),
+    )
 
 
 @missing_heatmap.register
 def spark_missing_heatmap(config: Settings, df: DataFrame) -> str:
-    return plot_missing_heatmap(config, MissingnoBarSparkPatch(df))
+    df = MissingnoBarSparkPatch(df, columns=df.columns, original_df_size=df.count())
+
+    # Remove completely filled or completely empty variables.
+    columns = [i for i, n in enumerate(np.var(df.isnull(), axis="rows")) if n > 0]
+    df = df.iloc[:, columns]
+
+    # Create and mask the correlation matrix. Construct the base heatmap.
+    corr_mat = df.isnull().corr()
+    mask = np.zeros_like(corr_mat)
+    mask[np.triu_indices_from(mask)] = True
+    return plot_missing_heatmap(
+        config, corr_mat=corr_mat, mask=mask, columns=list(df.columns)
+    )