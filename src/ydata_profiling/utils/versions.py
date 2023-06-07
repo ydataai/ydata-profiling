4284fda4baa790412effa2c099e8647c4cf5f874
Alexandre Quemy <alexandre.quemy@gmail.com>
2023-06-07 14:21:28 +0200

    feat: udpate type inference for pandas>=2

---
+++ src/ydata_profiling/utils/versions.py
@@ -0,0 +1,10 @@
+from importlib.metadata import version
+
+def pandas_version() -> list:
+    return map(int, version('pandas').split('.'))
+
+def pandas_major_version() -> int:
+    return pandas_version()[0]
+
+def is_pandas_1() -> bool:
+    return pandas_major_version == 1
