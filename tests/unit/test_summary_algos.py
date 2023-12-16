c8d253a2cb531f459df86fbe6924b3628b8662c4
Jan Cap <jan.cap@profinit.eu>
2023-12-16 19:32:28 +0100

    feat: update tests

--- tests/unit/test_summary_algos.py
+++ tests/unit/test_summary_algos.py
@@ -4,35 +4,37 @@ import pytest
 
 from ydata_profiling.config import Settings
 from ydata_profiling.model.summary_algorithms import (
-    describe_counts,
-    describe_generic,
     describe_supported,
     histogram_compute,
 )
+from ydata_profiling.model.var_description.default import VarDescription
 
 
 def test_count_summary_sorted(config):
     s = pd.Series([1] + [2] * 1000)
-    _, sn, r = describe_counts(config, s, {})
-    assert r["value_counts_without_nan"].index[0] == 2
-    assert r["value_counts_without_nan"].index[1] == 1
+    r: VarDescription
+    _, sn, r = describe_supported(config, s, {})
+    assert r.value_counts_without_nan.index[0] == 2
+    assert r.value_counts_without_nan.index[1] == 1
 
 
 def test_count_summary_nat(config):
+    r: VarDescription
     s = pd.to_datetime(pd.Series([1, 2] + [np.nan, pd.NaT]))
-    _, sn, r = describe_counts(config, s, {})
-    assert len(r["value_counts_without_nan"].index) == 2
+    _, sn, r = describe_supported(config, s, {})
+    assert len(r.value_counts_without_nan.index) == 2
 
 
 def test_count_summary_category(config):
+    r: VarDescription
     s = pd.Series(
         pd.Categorical(
             ["Poor", "Neutral"] + [np.nan] * 100,
             categories=["Poor", "Neutral", "Excellent"],
         )
     )
-    _, sn, r = describe_counts(config, s, {})
-    assert len(r["value_counts_without_nan"].index) == 2
+    _, sn, r = describe_supported(config, s, {})
+    assert len(r.value_counts_without_nan.index) == 2
 
 
 @pytest.fixture(scope="class")
@@ -41,16 +43,12 @@ def empty_data() -> pd.DataFrame:
 
 
 def test_summary_supported_empty_df(config, empty_data):
-    _, series, summary = describe_counts(config, empty_data["A"], {})
-    assert summary["n_missing"] == 0
-    assert "p_missing" not in summary
-
-    _, series, summary = describe_generic(config, series, summary)
-    assert summary["n_missing"] == 0
-    assert summary["p_missing"] == 0
-    assert summary["count"] == 0
-
-    _, _, summary = describe_supported(config, series, summary)
+    summary: VarDescription
+    _, _, summary = describe_supported(config, empty_data["A"], {})
+    assert summary.n_missing == 0
+    assert summary.n_missing == 0
+    assert summary.p_missing == 0
+    assert summary.count == 0
     assert summary["n_distinct"] == 0
     assert summary["p_distinct"] == 0
     assert summary["n_unique"] == 0
