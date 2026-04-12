1e2fa10eaf7a951acea663fa270784244ac18404
Pkcha <pkcha@PkchadeMacBook-Air.local>
2026-04-12 19:27:37 +0800

    feat: initial release

--- src/ydata_profiling/model/summarizer.py
+++ src/ydata_profiling/model/summarizer.py
@@ -27,7 +27,7 @@ from ydata_profiling.model.pandas import (
 from ydata_profiling.model.pandas.describe_supported_pandas import (
     pandas_describe_supported,
 )
-from ydata_profiling.model.summary_algorithms import (
+from ydata_profiling.model.summary_algorithms import (  # Check what is this method used for
     describe_file_1d,
     describe_image_1d,
     describe_path_1d,
@@ -50,8 +50,9 @@ class BaseSummarizer(Handler):
         return self.handle(str(dtype), config, series, {"type": str(dtype)})
 
 
+# Revisit this with the correct support for Spark as well.
 class ProfilingSummarizer(BaseSummarizer):
-    """A summarizer supporting both Pandas and Spark DataFrames."""
+    """A summarizer for Pandas DataFrames."""
 
     def __init__(self, typeset: VisionsTypeset, use_spark: bool = False):
         self.use_spark = use_spark and is_pyspark_installed()
